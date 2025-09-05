6ee52d908a2be1ce4f3740e77aec3a2fcfd2def7
Theodor Ampas <211319935+dev-dmm@users.noreply.github.com>
2025-09-05 17:39:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WooCommerceOrderController;
 
-Route::post('/woocommerce/order', [WooCommerceOrderController::class, 'store'])
-    ->name('api.woocommerce.order')
-    ->middleware('throttle:60,1'); // later you can swap/add Sanctum/HMAC/etc.
+Route::prefix('woocommerce')
+    ->name('api.woocommerce.')
+    ->middleware('throttle:60,1')
+    ->group(function () {
+        Route::post('/order', [WooCommerceOrderController::class, 'store'])
+            ->name('order');
+    });
