25bc297374d7821d5bec748843f70af0a1518b87
Theodor Ampas <211319935+dev-dmm@users.noreply.github.com>
2025-09-10 12:45:13 +0300

    quick fix design test

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -69,7 +69,7 @@ export default function AuthenticatedLayout({ header, children }) {
                                 
                                 {/* Super Admin Navigation */}
                                 {isSuperAdmin() && (
-                                    <div className="relative">
+                                    <div className="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700">
                                         <Dropdown>
                                             <Dropdown.Trigger>
                                                 <button className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
