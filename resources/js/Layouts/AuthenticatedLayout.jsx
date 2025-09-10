a9184fe377dc561cc2e0c0cdaeeec95d998652de
Theodor Ampas <211319935+dev-dmm@users.noreply.github.com>
2025-09-10 17:48:18 +0300

    test push

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -166,7 +166,6 @@ export default function AuthenticatedLayout({ header, children }) {
           <div className="mx-auto container px-4 py-6 sm:px-6 lg:px-8">{header}</div>
         </header>
       )}
-
       <main className="container">{children}</main>
     </div>
   );
