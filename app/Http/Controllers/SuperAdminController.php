46fd674b03e3227d496b47fe5877366380c6343b
Theodor Ampas <211319935+dev-dmm@users.noreply.github.com>
2025-09-15 17:07:24 +0300

    update dashbaord

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -138,7 +138,7 @@ class SuperAdminController extends Controller
             ->having('orders_count', '>', 0)
             ->orderBy('orders_count', 'desc')
             ->limit(10)
-            ->select(['id','name','subdomain','orders_count']);
+            ->select(['id','name','subdomain']);
     
         return Inertia::render('SuperAdmin/Dashboard', [
             'stats'        => $stats,                               // eager
